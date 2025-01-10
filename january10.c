9f858bcc5b5eaaa0e507543a42cec4278b0042f1
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2025-01-10 16:24:40 -0500

    weeoooeeeooo

---
+++ january10.c
@@ -0,0 +1,80 @@
+// NOTES 1/10
+// C REVIEW + DYNAMIC MEMORY ALLOCATION
+
+/*
+DYNAMIC MEMORY ALLOCATION:
+- Number of values is allowed to be changed
+- Not static
+- Memory storage takes place in heap space
+- Memory requirements are unknown at COMPILE TIME
+- No name directly to the allocated memory BUT the pointer has a name
+- Isn't freed AUTOMATICALLY
+- Takes space in heap space
+- Computer warns if there's no space when we're trying to malloc
+- Requires stdlib.h
+
+STATIC MEMORY:
+- Space is known during COMPILE TIME
+- Name of var is required
+- Takes up space regardless of whether it was used or not, may hold garbage values though
+- Memory storage takes place in stack space
+- If value is declared in function, its memory and values are destroyed (freed) once the function is finished running (poof!)
+- Freed automatically once the function ends
+
+WHY WE CANNOT RETURN STATIC ARRAYS:
+- Because after the function is ran, the array's data is freed, all the new value in the main function holds is the memory address of that array, which has no more values! 
+- SOOOO we can't return static array
+
+*/
+
+#include <stdio.h>
+#include <sys/_types/_null.h>
+#include <stdlib.h> // HOLDS DYNAMIC MEMORY FUNCTIONS
+
+void makeNULLTest(int **p) {
+    *p = NULL;
+}
+
+void mainTest() {
+    int *q;
+    makeNULLTest(&q);
+
+}
+
+// IMPOSSIBLE, CANT DO
+// int * returnArr() {
+//     int arr[10];
+//     arr[0]=4;
+//     return arr; 
+
+// }
+
+int main() {
+    // Making Q ptr NULL
+    mainTest();
+
+    // int arr[10] = returnArr();
+
+    /*
+    STATIC MEMORY AND SCANF
+    - We don't know what size is
+    - Uses memory pool called STACK to store this memory and it is actually limited
+    - Likely the program would crash if too big
+    - Similar to other statically allocated memory, the var would be freed after the function is ran
+    */
+    int size;
+    printf("Enter size: ");
+    scanf("%d", &size);
+
+    // DYNAMIC MEMORY ALLOCATION
+    /*
+    
+    */
+    int * test = malloc(sizeof(int)*10); // Requires bytes in parameter, makes test point to a space in the heap space with 40 bytes
+    test[0] = 7;
+
+
+    return 0;
+
+
+}
